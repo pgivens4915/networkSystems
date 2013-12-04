d1255db56c916be399c763237ed1c2209c8ed2d0
pgivens4915 <pgivens4915@gmail.com>
2013-12-04 12:15:48 -0700

    Wrote the shell of the server
    It should accept a connection, receive a message and respond with a modified
    message

--- server.c
+++ server.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <strings.h>
 
 int main(int argc, char* argv[]){
-  printf("Hello server\n");
+  int listenFd;
+  int clientFd;
+  int size;
+  int length;
+  struct sockaddr_in serverAddr;
+  struct sockaddr_in clientAddr;
+  socklen_t clientLen;
+  char mesg[1024] = "TEST\n";
+
+  listenFd = socket(AF_INET, SOCK_STREAM, 0);
+
+  bzero(&serverAddr, sizeof(serverAddr));
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serverAddr.sin_port = htons(9000);
+  
+  bind(listenFd, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+  listen(listenFd, 1024);
+
+  for(;;){
+    clientLen = sizeof(clientAddr);
+    clientFd = accept(listenFd, (struct sockaddr *) &clientAddr, &clientLen);
+    // Receiving bits
+    length = sizeof(clientAddr);
+    size = recvfrom(clientFd, mesg, 1024, 0, (struct sockaddr *) &clientAddr,
+                    &length);
+
+    mesg[0] = '9';
+    sendto(clientFd, mesg, size, 0, (struct sockaddr *) &clientAddr, length);
+    printf("Transfer done");
+    
+  }
 }
